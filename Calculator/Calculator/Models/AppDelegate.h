b5da18e9ff828352a37e0654366998ba8ed74b20
Alex <1920176@00-63159.pusd.dom>
2019-10-22 09:45:22 -0700

    done
    p

---
+++ Calculator/Calculator/Models/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by John Mortensen on 10/10/19.
+//  Copyright © 2019 JM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
